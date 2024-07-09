f3b4b341764b837415bb06e71f09b2fed1f809b1
James P. Galovic <jpgalovic@gmail.com>
2024-07-09 18:09:05 +1000

    Change Header and Footer to Generic Using Javascript

---
+++ elements/footer.js
@@ -0,0 +1,15 @@
+
+footer.innerHTML = `
+<div class="inner">
+    <ul class="icons">
+        <li><a href="https://www.youtube.com/channel/UCjlUcCtyqn2zmBEuWftgg8Q" class="icon brands alt fa-youtube"><span class="label">Youtube</span></a></li>
+        <li><a href="https://www.facebook.com/SpiceOfLifeEnt/" class="icon brands alt fa-facebook-f"><span class="label">Facebook</span></a></li>
+        <li><a href="https://www.instagram.com/spice.of.life.entertainment/" class="icon brands alt fa-instagram"><span class="label">Instagram</span></a></li>
+        <li><a href="https://open.spotify.com/playlist/7BqUool25Zi6PTSd6k00Tf?si=oTsD2TYJTc-UKXvB4zWL-w&nd=1&dlsi=8a22bd4c411a43e5" class="icon brands alt fa-spotify"><span class="label">Spotify</span></a></li>
+        <li><a href="https://soundcloud.com/spiceoflifeentertainment" class="icon brands alt fa-soundcloud"><span class="label">Soundcloud</span></a></li>
+    </ul>
+    <ul class="copyright">
+        <li>&copy; Copyright 2024, Spice of Life Entertainment. All rights reserved</li><li>Developer: <a href="https://entropyis.me">Entropy is Software Development</a></li><li>Design: <a href="https://html5up.net">HTML5 UP</a></li>
+    </ul>
+</div>
+`
---
+++ elements/header.js
@@ -0,0 +1,14 @@
+
+header.innerHTML = `
+    <a href="index.html" class="logo"><img src="images/logo_nt.png" /> <span>Spice of Life Entertainment</span></a>
+    <nav>
+        <a href="#menu">Menu</a>
+    </nav>
+`
+
+menu.innerHTML = `
+    <ul class="links">
+        <li><a href="index.html">Home</a></li>
+        <li><a href="artists.html">Artists</a></li>
+    </ul>
+`
