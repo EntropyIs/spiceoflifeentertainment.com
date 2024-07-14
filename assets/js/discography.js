728274720261010cde88dc45a4738828ecc5909c
James P. Galovic <jpgalovic@gmail.com>
2024-07-14 23:48:40 +1000

    Update discography.js

--- assets/js/discography.js
+++ assets/js/discography.js
@@ -6,5 +6,14 @@ console.log(artist);
 
 $.getJSON('assets/data/discography.json', function(data)
 {
-    console.log(data)
+    console.log(data);
+    var items = [];
+    $.each(data, function(key, val) {
+        items.push("<li id='" + key + "'>" + val + "</li>");
+    });
+
+    $( "<ul/>", {
+        "class" : "my-new-list",
+        html: items.join("")
+    }).appendTo("body");
 });
