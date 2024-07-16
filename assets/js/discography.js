a7c2c1c928c213192558a81dd65c1ce01455113c
James P. Galovic <jpgalovic@gmail.com>
2024-07-16 13:57:31 +1000

    Removed Contact from Adrian Page

--- assets/js/discography.js
+++ assets/js/discography.js
@@ -7,13 +7,4 @@ console.log(artist);
 $.getJSON('assets/data/discography.json', function(data)
 {
     console.log(data);
-    var items = [];
-    $.each(data, function(key, val) {
-        items.push("<li id='" + key + "'>" + val + "</li>");
-    });
-
-    $( "<ul/>", {
-        "class" : "my-new-list",
-        html: items.join("")
-    }).appendTo("body");
 });
